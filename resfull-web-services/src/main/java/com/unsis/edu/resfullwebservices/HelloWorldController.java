0ba20235824bd12b0259660b2dabc1763ea9a619
OctavioLuis <oct.luisg@gmail.com>
2019-12-19 20:54:29 -0600

    Creacion del proyecto

---
+++ resfull-web-services/src/main/java/com/unsis/edu/resfullwebservices/HelloWorldController.java
@@ -0,0 +1,5 @@
+package com.unsis.edu.resfullwebservices;
+
+public class HelloWorldController {
+
+}
---
+++ resfull-web-services/src/test/java/com/unsis/edu/resfullwebservices/ResfullWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unsis.edu.resfullwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResfullWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
